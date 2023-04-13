5f22f619d80f7ce9977946ef48b52248118db952
clbrunet <brunet.clement5@gmail.com>
2023-04-13 16:35:55 +0200

    feat: add a python script to create a release build

---
+++ release_build.py
@@ -0,0 +1,17 @@
+import os
+import subprocess
+import sys
+
+def main():
+    if len(sys.argv) > 2:
+        print("usage:\n\tpython3 release_build.py [release_build_dir]")
+        return
+    release_build_dir = 'release_build'
+    if len(sys.argv) > 1:
+        release_build_dir = sys.argv[1]
+    os.makedirs(release_build_dir, exist_ok=True)
+    subprocess.check_call('cmake -DCMAKE_BUILD_TYPE=Release -B ' + release_build_dir, shell=True)
+    subprocess.check_call('cmake --build .', shell=True, cwd=release_build_dir)
+
+if __name__ == '__main__':
+    main()
